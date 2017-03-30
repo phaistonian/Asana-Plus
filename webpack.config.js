ca5703b48896765be06b76828bc52d9bf2419731
George Papadakis <phaistonian@gmail.com>
2017-03-30 21:04:39 +0300

    fixes

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    background: './src/background.js',
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+
+  resolve: {
+    alias: {
+      react: 'react-lite',
+      'react-dom': 'react-lite',
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        include: /.\//,
+        exclude: /.\/dist/,
+        enforce: 'pre',
+      },
+      {
+        test: /\.js$/,
+        include: /.\//,
+        exclude: /.\/dist/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.styl$/,
+        include: /.\//,
+        exclude: /.\/dist/,
+        loader: ExtractTextPlugin.extract({
+          notExtractLoader: 'style-loader',
+          loader: ['css-loader', 'stylus-loader?{"resolve url":1}'],
+        }),
+      },
+    ],
+  },
+
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'index.css',
+      allChunks: true,
+    }),
+
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        eslint: {
+          failOnError: true,
+        },
+      },
+    }),
+  ],
+};
