624687d8fe0cfef005886274e87d83ffe43ca671
George Papadakis <phaistonian@gmail.com>
2017-04-20 08:35:08 +0300

    fixes

--- dist/index.js
+++ dist/index.js
@@ -148,7 +148,9 @@ Object.defineProperty(exports, "__esModule", {
 const REG = /^\[(\d+?\w{0,1})\]/;
 
 const getRowValue = row => {
-  const content = row.querySelector('textarea').value;
+  const target = row.querySelector('textarea');
+
+  const content = target.nodeName === 'TEXTARA' ? target.value : target.textContent;
 
   console.log(content);
 
@@ -157,37 +159,61 @@ const getRowValue = row => {
     return value;
   }
 
-  return 0;
+  return false;
 };
 
 const getTotalFromRows = rows => rows.reduce((acc, row) => acc + getRowValue(row), 0) || '';
 
-const updateTotals = rows => {
-  const header = document.querySelector('.details-pane-title .header-name.read-only');
+const getHeader = () => {
+  const headers = [...document.querySelectorAll('.details-pane-title .header-name.read-only, .MultiTaskTitleRow-titleText')];
+
+  if (!headers.length) {
+    console.log('Header not found');
+  }
+
+  const header = headers[0];
 
   if (!header) {
-    return;
+    return false;
   }
 
+  return header;
+};
+
+const updateTotals = rows => {
   const total = getTotalFromRows(rows);
 
-  if (total) {
-    header.dataset.total = `[${total}] `;
-  } else {
-    Reflect.deleteProperty(header.dataset, 'total');
-  }
+  setTimeout(() => {
+    const header = getHeader();
+
+    if (!header) {
+      console.log('header not found');
+      return;
+    }
+
+    console.log('total', total);
+
+    if (total) {
+      header.dataset.total = `[${total}] `;
+    } else {
+      Reflect.deleteProperty(header.dataset, 'total');
+    }
+  }, 100);
 };
 
 const checkForMultipleSelectedTasks = () => {
-  const selectedRows = [...document.querySelectorAll('#grid tr.grid-row-selected')];
+  const selectedRows = [...document.querySelectorAll('#grid tr.grid-row-selected, .itemRow--highlighted, .TaskRow--focused')];
 
+  console.log(1, selectedRows.length);
   if (selectedRows.length > 1) {
     updateTotals(selectedRows);
   }
 };
 
 const init = () => {
-  document.addEventListener('mouseup', checkForMultipleSelectedTasks);
+  document.addEventListener('mouseup', () => {
+    setTimeout(() => requestAnimationFrame(checkForMultipleSelectedTasks), 50);
+  });
 };
 
 exports.default = init;
